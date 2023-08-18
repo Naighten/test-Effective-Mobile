d33412a152e29ded75ac80468f877fd48668c930
AlexZh <aleksandr52.02@mail.ru>
2023-08-18 11:33:52 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { PostspageComponent } from './components/postspage/postspage.component';
+import { PostpageComponent } from './components/postpage/postpage.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    PostspageComponent,
+    PostpageComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
